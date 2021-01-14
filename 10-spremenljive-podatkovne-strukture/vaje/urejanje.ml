24178e77cd1b1ef0ac6e1f5e62ae4725317ef16b
MancaStrah <manca.strah@gmail.com>
2021-01-14 18:30:40 +0100

    vaje

--- 10-spremenljive-podatkovne-strukture/vaje/urejanje.ml
+++ 10-spremenljive-podatkovne-strukture/vaje/urejanje.ml
@@ -9,6 +9,12 @@
  val l : int list = [0; 1; 0; 4; 0; 9; 1; 2; 5; 4]
 [*----------------------------------------------------------------------------*)
 
+let rec randlist len max = 
+	if len <= 0 then 
+		[]
+	else 
+		let x = Random.int max in 
+		x :: randlist (len - 1) max
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Sedaj lahko s pomočjo [randlist] primerjamo našo urejevalno funkcijo (imenovana
@@ -18,6 +24,9 @@
  let test = (randlist 100 100) in (our_sort test = List.sort compare test);;
 [*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
 
+let default_test our_sort = 
+	let test = randlist 100 100 in 
+	our_sort test = List.sort compare test
 
 (*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
  Urejanje z Vstavljanjem
@@ -35,24 +44,87 @@
  - : int list = [7]
 [*----------------------------------------------------------------------------*)
 
+let rec insert y xs = 
+	match xs with 
+	| [] ->  y :: [] 
+	| e :: es -> (
+		if y <= e then y :: xs
+		else e :: (insert y es)
+	)
 
 (*----------------------------------------------------------------------------*]
  Prazen seznam je že urejen. Funkcija [insert_sort] uredi seznam tako da
  zaporedoma vstavlja vse elemente seznama v prazen seznam.
 [*----------------------------------------------------------------------------*)
 
+let rec insert_sort = function
+	| [] -> []
+	| x :: xs -> insert x (insert_sort xs)
 
+
+let insert_sort_pro = List.fold_left (fun list x -> insert x list) [] 
+
+
+let insert_sort' list =	
+	let rec aux sorted list = 
+		match list with
+		| [] -> sorted
+		| x :: xs -> aux (insert x sorted) xs
+	in 
+	aux [] list
 
 (*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
  Urejanje z Izbiranjem
 [*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
+
 
 (*----------------------------------------------------------------------------*]
  Funkcija [min_and_rest list] vrne par [Some (z, list')] tako da je [z]
  najmanjši element v [list] in seznam [list'] enak [list] z odstranjeno prvo
  pojavitvijo elementa [z]. V primeru praznega seznama vrne [None]. 
 [*----------------------------------------------------------------------------*)
+ 
+let min_and_rest' list =
+	let rec aux min rest =  function 
+		| [] -> Some (min, rest)
+		| x :: xs -> (
+			if x < min then aux x (min :: rest) xs else
+			aux min (x :: rest) xs 
+		)
+		in 
+		match list with 
+		| [] -> None
+		| x :: xs -> aux x [] xs
 
+(* let min_and_rest list = 
+	let rec aux min rest list = 
+		match list with 
+		| [] -> Some (min, List.rev rest)
+		| x :: xs -> (
+			if x < min then aux x (min :: rest) xs else
+			 aux min (x :: rest) xs
+		)
+	in
+	match list with 
+	| [] -> None 
+	| x :: xs -> aux x [] xs
+ *)
+
+
+(* Tale pa ne pomeša seznama rest, ampak samo odstrani najmanjši element. *)
+let min_and_rest list =
+	match list with
+	| [] -> None
+	| x :: xs -> (
+		let min_x = List.fold_left min x xs in
+		let rec remove_min = function
+			| [] -> [] 
+			| y :: ys when y == min_x -> ys  
+			| y :: ys (* when y <> min_x *) -> y :: (remove_min ys)
+			in 
+		Some (min_x, remove_min list)
+	)
+	 
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Pri urejanju z izbiranjem na vsakem koraku ločimo dva podseznama, kjer je prvi
@@ -72,10 +144,16 @@
  Namig: Uporabi [min_and_rest] iz prejšnje naloge.
 [*----------------------------------------------------------------------------*)
 
-
+let rec selection_sort list =
+	let rec selection_loop sorted_list list = 
+		match min_and_rest list with
+		| Some (m, rest) -> selection_loop (m :: sorted_list) rest
+		| None -> List.rev sorted_list
+	in 
+	selection_loop [] list
 
 (*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
- Urejanje z Izbiranjem na Tabelah
+ Urejanje z Izbiranjem na Tabelah (Inplace selection sort )
 [*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
@@ -101,13 +179,25 @@
  - : int array = [|0; 4; 2; 3; 1|]
 [*----------------------------------------------------------------------------*)
 
+let swap a i j = 
+	let prvi = a.(i) in 
+	a.(i) <- a.(j);
+	a.(j) <- prvi 
 
 (*----------------------------------------------------------------------------*]
  Funkcija [index_min a lower upper] poišče indeks najmanjšega elementa tabele
  [a] med indeksoma [lower] and [upper] (oba indeksa sta vključena).
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- index_min [|0; 2; 9; 3; 6|] 2 4 = 4
+ index_min [|0; 2; 9; 3; 6|] 2 4 = 3
 [*----------------------------------------------------------------------------*)
+
+let rec index_min a lower upper = 
+  if lower >= upper then 
+    lower
+  else
+    let i_min = index_min a (lower + 1) upper in 
+    if a.(lower) < a.(i_min) then lower else i_min    
+    
 
 
 (*----------------------------------------------------------------------------*]
@@ -116,4 +206,15 @@
  Namig: Za testiranje uporabi funkciji [Array.of_list] in [Array.to_list]
  skupaj z [randlist].
 [*----------------------------------------------------------------------------*)
+
+let selection_sort_array a = 
+  let len = Array.length a in 
+  let rec sort_loop (boundary : int) = 
+    if boundary >= len then () 
+    else 
+      let i_min = index_min a boundary (len - 1) in 
+      swap a boundary i_min;
+      sort_loop (boundary + 1)
+      in
+  sort_loop 0
 
--- 11-deli-in-vladaj/vaje/deli_in_vladaj.py
+++ 11-deli-in-vladaj/vaje/deli_in_vladaj.py
@@ -24,11 +24,55 @@
 #     >>> pivot(a, 1, 7)
 #     3
 #     >>> a
-#     [10, 2, 0, 4, 11, 15, 17, 5, 18]
+#     [10, 2, 0, 4, 11, 15, 17, 5, 18]  
+
 ###############################################################################
 
 
+# OK rešitev
 
+def pivot(a, start, end):
+    if start == end:
+        return start
+    p = a[start]
+    prvi_vecji = start + 1
+    for i in range (start + 1, end + 1):
+        if a[i] < p:
+            a[prvi_vecji], a[i] = a[i], a[prvi_vecji]
+            prvi_vecji += 1
+    a[start], a[prvi_vecji - 1] = a[prvi_vecji - 1], a[start]
+    return prvi_vecji - 1
+
+
+
+
+# Patetični poskus, ne deluje ok za dva elementa.
+# Bomo enkrat mičken kasneje ugotovili, kje tiči problemček. 
+# Ker je ura velik. 
+def pivot1(a, start, end):
+    if start == end:
+        return start
+    p = a[start]
+    biggest = a[start + 1]
+    m = 1
+    for i in range(start, end + 1): 
+        if a[i] <= p: 
+            a[start + m] = a[i]
+            a[i] = biggest 
+            m += 1
+            biggest = a[start+m]
+            if i == end:
+                a[start] = a[start + m - 1]
+                a[start + m - 1] = p
+        else:
+            if i == end:
+                a[start], a[start + m - 1] = a[start + m - 1], p
+    return start + m - 1
+            
+b = [2,1]
+a = [10, 4, 5, 15, 11, 2, 17, 0, 18]
+
+### TO SMO NA VAJAH PRESKOČILI ################################################
 ###############################################################################
 # V tabeli želimo poiskati vrednost k-tega elementa po velikosti.
 #
@@ -61,6 +105,66 @@
 ###############################################################################
 
 
+def quicksort(a):
+    def quicky(start, end):
+        if end - start <= 0:
+            return
+        p_index = pivot(a, start, end)
+        quicky(start, p_index - 1)
+        quicky(p_index + 1, end)
+    quicky(0, len(a)-1)
+    return
+
+
+
+
+# Tudi quicksort2 se zdi, da dela ok. Ni pa eleganten. 
+# Oziroma en korak preveč.
+
+def quicksort_part(a, start, end):
+    if end - start <= 0:
+
+        return
+    # if len(a) == 1:
+        # return a
+    else: 
+        p_index = pivot(a, start, end)
+        quicksort_part(a, start, p_index - 1)
+        quicksort_part(a, p_index + 1, end)
+
+
+def quicksort2(a):
+    p_index = pivot(a, 0, len(a))
+    quicksort_part(a, 0, p_index - 1)
+    quicksort_part(a, p_index + 1, len(a))
+
+
+import random
+def test_quicksort(n, max_l, max_k):
+    for _ in range(n):
+        l = random.randint(0, max_l)
+        a = [random.randint(-max_k, max_k) for _ in range(l)]
+        a1 = a[:]
+        a2 = a[:]
+        quicksort(a1)
+        a2.sort()
+        if a1 != a2:
+            print(a)
+    return          
+
+
+def test_quicksort2(n, max_l, max_k):
+    for _ in range(n):
+        l = random.randint(0, max_l)
+        a = [random.randint(-max_k, max_k) for _ in range(l)]
+        a1 = a[:]
+        a2 = a[:]
+        quicksort2(a1)
+        a2.sort()
+        if a1 != a2:
+            print(a)
+    return    
+
 
 ###############################################################################
 # Če imamo dve urejeni tabeli, potem urejeno združeno tabelo dobimo tako, da
@@ -85,7 +189,26 @@
 #
 ###############################################################################
 
+def zlij(target, begin, end, list_1, list_2):
+    i1 = 0
+    i2 = 0
+    for j in range(begin, end + 1):
+        i1_is_ok = i1 < len(list_1)
+        i2_is_ok = i2 < len(list_2)
+        if (not i2_is_ok and i1_is_ok) or (i1_is_ok and list_1[i1] <= list_2[i2]):
+            target[j] = list_1[i1]
+            i1 += 1
+        elif i2_is_ok:
+            target[j] = list_2[i2]
+            i2 += 1
+        else: 
+            raise Exception("Lengths of lists are not sufficient")
 
+# list_1 = [1,3,5,7,10]
+# list_2 = [1,2,3,4,5,6,7]
+# target = [-1 for _ in range(len(list_1) + len(list_2))]
+# zlij(target, 0, len(target) - 1, list_1, list_2)
+# target
 
 ###############################################################################
 # Tabelo želimo urediti z zlivanjem (merge sort). 
@@ -102,3 +225,26 @@
 # >>> mergesort(a)
 # [2, 3, 4, 5, 10, 11, 15, 17, 18]
 ###############################################################################
+
+def mergesort(a):
+    if len(a) <= 1:
+        return
+    half = len(a) // 2 
+    a1 = a[:half]
+    a2 = a[half:]
+    mergesort(a1)
+    mergesort(a2)
+    zlij(a, 0, len(a) - 1, a1, a2)
+
+
+def test_mergesort(n, max_l, max_k):
+    for _ in range(n):
+        l = random.randint(0, max_l)
+        a = [random.randint(-max_k, max_k) for _ in range(l)]
+        a1 = a[:]
+        a2 = a[:]
+        mergesort(a1)
+        a2.sort()
+        if a1 != a2:
+            print(a)
+    return 
